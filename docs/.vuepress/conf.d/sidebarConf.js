0e1ae3d4876881f65e7d2a9344cdd5e57ad3397e
WangYan <no-reply@wangyan.org>
2020-11-07 02:11:55 +0800

    更新 1.1.0 简介

--- docs/.vuepress/conf.d/sidebarConf.js
+++ docs/.vuepress/conf.d/sidebarConf.js
@@ -1,3 +1,16 @@
 module.exports = {
-  //
+  '/1-js/': [ 
+    {
+      title: '第 1 节：简介',
+      path: '/1-js/01-getting-started/',
+      collapsable: false,
+      sidebarDepth: 1,
+      children: [
+        ['/1-js/01-getting-started/1-intro/article', '1.1. JavaScript 简介'],
+        ['/1-js/01-getting-started/2-manuals-specifications/article', '1.2. 手册与规范'],
+        ['/1-js/01-getting-started/3-code-editors/article', '1.3. 代码编辑器'],
+        ['/1-js/01-getting-started/4-devtools/article', '1.4. 开发者控制台'],
+      ]
+    },
+  ],
 };
