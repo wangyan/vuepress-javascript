7e656fcc1b9719476d62a4348623a1e99c2c0070
WangYan <no-reply@wangyan.org>
2021-07-10 16:31:14 +0800

    配置：gh-pages 使用自定义域名

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,6 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 当前日期与时间
+DateTimes=$(date '+%Y-%m-%d %H:%M:%S')
+
 # 生成本地静态文件
 yarn docs:build
 
@@ -11,7 +14,7 @@ rm -rf gh-pages/
 git clone git@github.com:wangyan/javascript-tutorial.git gh-pages
 cd gh-pages/
 git checkout gh-pages
-git reset --hard 0d7b08
+git reset --hard ed08fcb
 
 # 初始化 Git
 git config user.name WangYan
@@ -20,7 +23,7 @@ git config user.email no-reply@wangyan.org
 # Git 提交
 mv ../docs/.vuepress/dist/* .
 git add -A
-git commit -m 'Deploy Code'
+git commit -m "Last update: $DateTimes"
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push --force git@github.com:<USERNAME>/<USERNAME>.github.io.git master
