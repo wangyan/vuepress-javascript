ce88fe48815a156bcf6fae8e379417eae5538cd8
WangYan <no-reply@wangyan.org>
2021-10-21 12:17:25 +0000

    配置：使用 Access Token 推送 gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -9,12 +9,17 @@ yarn docs:build
 # 初始化 Git
 git config user.name WangYan
 git config user.email no-reply@wangyan.org
+git config commit.gpgsign true
+git config user.signingkey 70B152332E0E8722
+
+#sudo apt-get install gnupg2 -y
+curl ${GPG_PRIVATE_KEY} | gpg --import
+curl ${GPG_PUBLIC_KEY} | gpg --import
 
 # 克隆远程仓库
 rm -rf gh-pages/
-git clone git@github.com:wangyan/javascript-tutorial.git gh-pages
+git clone -b gh-pages https://github.com/wangyan/javascript-tutorial.git gh-pages
 cd gh-pages/
-git checkout gh-pages
 git reset --hard ed08fcb
 
 # Git 提交
@@ -27,10 +32,8 @@ git commit -m "Last update: $(date '+%Y-%m-%d %H:%M:%S')"
 # git push --force git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push --force git@github.com:wangyan/javascript-tutorial.git gh-pages:gh-pages
-
-# 如果使用 travis 持续集成
-# git push --force https://${username}:${access_token}@github.com/wangyan/javascript-tutorial.git gh-pages:gh-pages
+# git push --force git@github.com:wangyan/javascript-tutorial.git gh-pages:gh-pages
+git push --force https://${GIT_AUTHOR_NAME}:${GIT_ACCESS_TOKEN}@github.com/wangyan/javascript-tutorial.git gh-pages:gh-pages
 
 # 清理
 cd - 
